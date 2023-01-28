dccc4a4301f9f12c4de9b7abdc73ad14896601cd
2004shashwat17 <114841105+2004shashwat17@users.noreply.github.com>
2023-01-28 23:25:15 +0530

    I have made some logical changes

--- src/calculator.java
+++ src/calculator.java
@@ -1,7 +1,40 @@
 import java.util.Scanner;
 public class calculator {
     public static void main(String[]args){
+        Scanner in = new Scanner(System.in);
+        int ans = 0;
+        while (true){
+            System.out.println("Enter the operator: ");
+            char op = in.next().trim().charAt(0);
 
-    }
+            if (op == '+' || op == '-' || op == '*' || op == '/'|| op == '%') {
+                System.out.println("Enter two numbers: ");
+                int num1 = in.nextInt();
+                int num2 = in.nextInt();
 
+                if ( op == '+') {
+                    ans = num1 + num2;
+                }
+                if (op == '-') {
+                    ans = num1 - num2;
+                }
+                if (op == '*') {
+                    ans = num1 * num2;
+                }
+                if (op == '/') {
+                    if (num2 != 0) {
+                        ans = num1 / num2;
+                    }
+                }
+                if (op =='%') {
+                    ans = num1 % num2;
+                }
+            } else if ( (op == 'x') || op == 'X') {
+                    break;
+            } else{
+                    System.out.println("Invalid operation!!");
+            }
+                System.out.println(ans);
+            }
+     }
 }
