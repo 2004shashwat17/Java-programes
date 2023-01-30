d08ba8b49b54200862ed64fb67690ff3bd17baaa
2004shashwat17 <114841105+2004shashwat17@users.noreply.github.com>
2023-01-30 09:59:23 +0530

    I have made some logical changes

--- src/PalindromeString.java
+++ src/PalindromeString.java
@@ -1,1 +1,21 @@
 
+import java.util.Scanner;
+public class PalindromeString{
+    public static void main(String[] args) {
+        Scanner in= new Scanner(System.in);
+        int num = in.nextInt();
+        int t= num;
+        int rev=0;
+
+        while(num >0) {
+            rev = rev * 10 + num % 10;
+            num = num / 10;
+        }
+        if(t==rev){
+            System.out.println("palindrome ");
+        } else{
+            System.out.println("not palindrome");
+        }
+
+    }
+}
