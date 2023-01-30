5c0f04de86ac50b67f0cb116295a8c489c03d1e5
2004shashwat17 <114841105+2004shashwat17@users.noreply.github.com>
2023-01-30 14:53:04 +0530

    I have made some logical changes

--- src/Armstrongno.java
+++ src/Armstrongno.java
@@ -1,22 +1,55 @@
+//THIS PROGRAM IS TO CHECK WHETHER THE GIVEN NUMBER IS ARMSTRONG OR NOT?
+//import java.util.Scanner;
+//public class Armstrongno {
+//    public static void main(String[] args) {
+//        Scanner in = new Scanner(System.in);
+//        int num= in.nextInt();
+//        int t=num;
+//        int sum=0;
+//
+//        while(num>0) {
+//            sum=sum+(num % 10)*(num % 10)*(num % 10);
+//            num = num / 10;
+//        }
+//        if(sum==t){
+//            System.out.println("the no.is armstrong");
+//        }
+//        else{
+//            System.out.println("the no is not an armstrong");
+//        }
+//
+//
+//    }
+//}
+//TO FIND ARMSTRONG NUMBER BETWEEEN TWO GIVEN NUMBERS?
 import java.util.Scanner;
 public class Armstrongno {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int num= in.nextInt();
-        int t=num;
-        int sum=0;
+        Scanner reader= new Scanner(System.in);
 
-        while(num>0) {
-            sum=sum+(num % 10)*(num % 10)*(num % 10);
-            num = num / 10;
-        }
-        if(sum==t){
-            System.out.println("the no.is armstrong");
-        }
-        else{
-            System.out.println("the no is not an armstrong");
-        }
+        System.out.println("Enter lower bound");
+        int lower= reader.nextInt();
+        System.out.println("Enter upper bound");
+        int upper= reader.nextInt();
+        for (int number=lower;number<upper;++number){
+            int digits=0;
+            int result=0;
+            int originalnumber=number;
+            //calculate number of digits
+            while(originalnumber!=0) {
+                originalnumber /= 10;
+                ++digits;
+            }
+            originalnumber=number;
+            while(originalnumber !=0) {
+                int remainder = originalnumber % 10;
+                result += Math.pow(remainder, digits);
+                originalnumber /= 10;
+            }
+            if(result==number){
+                System.out.println(number+"");
+            }
 
-
+        }
     }
 }
--- src/PalindromeString.java
+++ src/PalindromeString.java
@@ -1,21 +1,51 @@
-
+//To check whether the given number is a palindrome or not?
+//import java.util.Scanner;
+//public class PalindromeString{
+//    public static void main(String[] args) {
+//        Scanner in= new Scanner(System.in);
+//        int num = in.nextInt();
+//        int t= num;
+//        int rev=0;
+//
+//        while(num >0) {
+//            rev = rev * 10 + num % 10;
+//            num = num / 10;
+//        }
+//        if(t==rev){
+//            System.out.println("palindrome ");
+//        } else{
+//            System.out.println("not palindrome");
+//        }
+//
+//    }
+//}
+//q2)To check whether the given string is a palindrome or not?
 import java.util.Scanner;
 public class PalindromeString{
     public static void main(String[] args) {
+        String s=new String();
         Scanner in= new Scanner(System.in);
-        int num = in.nextInt();
-        int t= num;
-        int rev=0;
-
-        while(num >0) {
-            rev = rev * 10 + num % 10;
-            num = num / 10;
+        System.out.println("Enter the string to Check");
+        s=in.nextLine();
+        int i,j,flag=0;
+        i=0;
+        j=s.length()-1;
+        flag=0;
+        while(i<j && flag==0)
+        {
+            if(s.charAt(i)!=s.charAt(j))
+            {
+                flag = 1;
+                break;
+            }
+            i++;
+            j--;
         }
-        if(t==rev){
+        if(flag==0)
             System.out.println("palindrome ");
-        } else{
+         else
             System.out.println("not palindrome");
-        }
+
 
     }
 }
