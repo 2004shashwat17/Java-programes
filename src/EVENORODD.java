db59d45f16d56b051a401379803f3a741f2cc1de
2004shashwat17 <114841105+2004shashwat17@users.noreply.github.com>
2023-02-10 19:26:04 +0530

    calculate no of permutations and combinations?

--- src/EVENORODD.java
+++ src/EVENORODD.java
@@ -15,12 +15,13 @@ import java.util.Scanner;
 public class EVENORODD{
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        System.out.print("print a number:");
-        int num = reader.nextInt();
-        if (num % 2 == 0)
-            System.out.println(num + "is even");
-        else
-            System.out.println(num + "is odd");
+        while (true) {
+            System.out.print("print a number:");
+            int num = reader.nextInt();
+            if (num % 2 == 0)
+                System.out.println(num + "is even");
+            else
+                System.out.println(num + "is odd");
+        }
     }
-
 }
--- src/factorsofPOSITIVENEGATIVE.java
+++ src/factorsofPOSITIVENEGATIVE.java
@@ -1,3 +1,6 @@
+import javax.swing.plaf.metal.MetalBorders;
+import java.util.Scanner;
+
 //public class factorsofPOSITIVENEGATIVE {
 //
 //        public static void main(String[] args) {
@@ -25,5 +28,67 @@
                 }
             }
         }
+    }
+}
+public class FactorialofPOSITIVENEGATIVE{
+    public static void main(String[]args){
+        Scanner in = new Scanner(System.in);
+            int Sum=0;
+            int float=54;
+            intfloast==0
+                    int class main(i love a girl that have preety eyes, bhoot sundar i love him so much but ahe reject me
+                why god why god.
+                abb khabhi ky a ma pyaar kisi sa kar bhi payu ga.
+                please javab do
+                    please
+                please
+                please
+                please
+                please
+                                   ya  galat bhat ha bhagwan  mara dil tod ka kisi ayr ki nahi h0o sakti vo
+                                   bhot ho gaya ha abb\
+                asa tota rahana sa kya hi hoga
+                koi rasta do bhagwan please
+                please
+                please
+                please
+                [\please
+                please
+                please
+                please
+                please
+                please
+                please
+                please
+                please
+                please
+                please
+                please
+                pleass
+                please
+                pleaseplease
+                please
+                please
+                please
+                please
+                please
+                please
+                please
+                pldeasepleaseplease
+                please
+                please
+                plple
+
+        int scan volumeofcylinder
+
+        int prom night= wing;
+                    int head=head
+                int scanner= scanner;
+                int scanner= scanner;
+intr float scanner= scanner;
+
+          int float int int (System.in)
+                                   ]
+                ,)
     }
 }
