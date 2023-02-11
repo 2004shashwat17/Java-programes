dfb01c801079ad90a8a616316759331481f641fd
2004shashwat17 <114841105+2004shashwat17@users.noreply.github.com>
2023-02-11 18:43:12 +0530

    write a program to print area of a circle of radius entered by user by defining your own method.

---
+++ src/areaofcircle.java
@@ -0,0 +1,31 @@
+//area of circle using method ?
+//import java.util.Scanner;
+//import java.lang.*;
+//public class areaofcircle {
+//    public static void main(String []args){
+//        Scanner sc = new Scanner (System.in);
+//        System.out.println("enter the radius: ");
+//        int r= sc.nextInt();
+//        double area =A(r);
+//        System.out.println("area of the circle: "+area);
+//
+//    }
+//    static double A(int r){
+//        double area =Math.PI*(r*r);
+//        return area;
+//    }
+//
+//}
+//area of circle?
+import java.util.Scanner;
+import java.lang.*;
+public class areaofcircle {
+    public static void main(String []args){
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Entre the radius: ");
+        int r=sc.nextInt();
+        double area= Math.PI*(r*r);
+        System.out.println("Area of circle: "+area);
+
+    }
+}
