277619324f12fcb8841a5d9dfc82f30f32608ebf
2004shashwat17 <114841105+2004shashwat17@users.noreply.github.com>
2023-02-11 18:42:33 +0530

    Define a method that returns the product of two numbers entered by user?

---
+++ src/productof2nos.java
@@ -0,0 +1,28 @@
+//product of two numbers using methods?
+//import java.util.Scanner;
+//public class productof2nos {
+//    public static void main(String[] args){
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter 1 and 2nd number: ");
+//        int n1 = sc.nextInt();
+//        int n2 = sc.nextInt();
+//        int product = p(n1,n2);
+//        System.out.println("sum of 2 numbers:"+product);
+//    }
+//    static int p(int n1,int n2){
+//        int product =n1*n2;
+//        return product;
+//    }
+//}
+//product of two numbers without method?
+import java.util.Scanner;
+public class productof2nos {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter 1 and 2 no: ");
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
+        int product= n1*n2;
+        System.out.println("product of"+product);
+    }
+}
