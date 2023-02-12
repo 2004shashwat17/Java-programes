f3e2322950bed0eb7c98621751affe429f87ee32
2004shashwat17 <114841105+2004shashwat17@users.noreply.github.com>
2023-02-12 18:37:04 +0530

    calculate HCFand gcd?

--- src/HCFandGCD.java
+++ src/HCFandGCD.java
@@ -1,20 +1,44 @@
-//to calculate hcf and gcd
+//to calculate hcf and gcd?
+
+//import java.util.Scanner;
+// public class HCFandGCD {
+//     public static void main(String[] args) {
+//         Scanner in = new Scanner(System.in);
+//         System.out.println("Enter the two numbers: ");
+//         int a = in.nextInt();
+//         int b = in.nextInt();
+//
+//         int g = 0;
+//
+//         for (int i = 1; i <= a; i++) {
+//             if (a % i == 0 && b % i == 0)
+//                 g = i;
+//         }
+//         System.out.println("GCD =" + g);
+//     }
+//}
+//to calculate hcf by using method?
 import org.w3c.dom.ls.LSOutput;
 
 import java.util.Scanner;
- public class HCFandGCD {
-     public static void main(String[] args) {
-         Scanner in = new Scanner(System.in);
-         System.out.println("Enter the two numbers: ");
-         int a = in.nextInt();
-         int b = in.nextInt();
-
-         int g = 0;
-
-         for (int i = 1; i <= a; i++) {
-             if (a % i == 0 && b % i == 0)
-                 g = i;
+public class HCFandGCD {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first number: ");
+        int a = sc.nextInt();
+        System.out.println("Enter second number: ");
+        int b = sc.nextInt();
+        hcf(a,b);
+        System.out.println("hcf of a no : "+hcf(a,b));
+    }
+    static int hcf(int a,int b){
+        int hcf=0;
+        for(int i=1;i<=a||i<=b;i++) {
+            if (a%i==0 && b%i==0){
+                hcf = i;
          }
-         System.out.println("GCD =" + g);
-     }
+         }
+        return hcf ;
+    }
+
 }
