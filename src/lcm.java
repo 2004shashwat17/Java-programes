a9d86c430ecd239b68c50a9f8ebf517d5beb1548
2004shashwat17 <114841105+2004shashwat17@users.noreply.github.com>
2023-02-15 00:21:26 +0530

    lcm?

--- src/lcm.java
+++ src/lcm.java
@@ -1,19 +1,44 @@
 //lcm
+//import java.util.Scanner;
+//public class lcm {
+//    public static void main(String[] args) {
+//        int lcm;
+//        int i;
+//        Scanner in = new Scanner(System.in);
+//        int a= in.nextInt();
+//        int b= in.nextInt();
+//        int l = a>b?a:b;
+//        for(i=l;i<=a*b;i+=l){
+//            if(i%a==0&&i%b==0) {
+//                break;
+//            }
+//        }
+//        lcm = i;
+//        System.out.println("LCM is: "+lcm);
+//    }
+//}
+//lcm using method?
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.Scanner;
-public class lcm {
-    public static void main(String[] args) {
-        int lcm;
-        int i;
+public class lcm{
+    public static void main(String[]  args){
         Scanner in = new Scanner(System.in);
-        int a= in.nextInt();
-        int b= in.nextInt();
-        int l = a>b?a:b;
-        for(i=l;i<=a*b;i+=l){
-            if(i%a==0&&i%b==0) {
-                break;
-            }
-        }
-        lcm = i;
-        System.out.println("LCM is: "+lcm);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        lcm(a,b);
+        System.out.println("lcm:"+lcm(a,b));
     }
-}
+     static int lcm(int a,int b) {
+         int i;
+         int l = a > b ? a : b;
+         int lcm;
+         for (i = l; i <= a * b; i += l) {
+             if (i % a == 0 && i % b == 0) ;
+             break;
+         }
+         lcm = i;
+         System.out.println("enter lcm:" + lcm);
+         return lcm;
+     }
+        }
