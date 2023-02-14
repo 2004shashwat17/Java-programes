ac5caa89eb4cae5ae7c84acc809f2043836ae79d
2004shashwat17 <114841105+2004shashwat17@users.noreply.github.com>
2023-02-15 00:21:44 +0530

    leap year?

--- src/Leapyear.java
+++ src/Leapyear.java
@@ -1,17 +1,37 @@
 //leap year
+//import java.util.Scanner;
+//public class Leapyear {
+//    public static void main(String[] args) {
+//        int year;
+//        System.out.println("Enter the year: ");
+//        Scanner in = new Scanner(System.in);
+//        year= in.nextInt();
+//        if((year%4==0 && year%100!=0|| year%400==0)){
+//            System.out.println("Specified year is a leap year: ");
+//        }
+//        else{
+//            System.out.println("Specified year is not a leap year: ");
+//        }
+//
+//    }
+//}
+//leap year using method?
 import java.util.Scanner;
 public class Leapyear {
-    public static void main(String[] args) {
-        int year;
-        System.out.println("Enter the year: ");
+    public static void main(String[] args){
         Scanner in = new Scanner(System.in);
-        year= in.nextInt();
-        if((year%4==0 && year%100!=0|| year%400==0)){
-            System.out.println("Specified year is a leap year: ");
-        }
-        else{
-            System.out.println("Specified year is not a leap year: ");
-        }
+        System.out.println("Enter the year: ");
+            int year=in.nextInt();
+        System.out.println(leapyear(year));
+    }
+    static String leapyear(int  year){
+
+    if((year%4==0 && (year%100!=0 ||year%400==0))){
+        return "specified year is a leap year";
+    }
+    else{
+        return "specified year is not a leap year";
+    }
 
     }
 }
