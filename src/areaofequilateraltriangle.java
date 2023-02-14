d59c493ebb7ee2ef64b473eff85d99bd8e41566d
2004shashwat17 <114841105+2004shashwat17@users.noreply.github.com>
2023-02-15 00:20:19 +0530

    area of equilateral triangle?

--- src/areaofequilateraltriangle.java
+++ src/areaofequilateraltriangle.java
@@ -1,12 +1,26 @@
 //area of equlaterial triangle?
+//import java.util.Scanner;
+//public class areaofequilateraltriangle {
+//public static void main(String[] args)
+//{
+//        Scanner s= new Scanner(System.in);
+//        System.out.println("Enter the side of the Triangle:");
+//        double a = s.nextDouble();
+//        double  area=(Math.sqrt(3)/4)*(a*a);
+//        System.out.println("Area of Triangle is: " + area);
+//        }
+//        }
 import java.util.Scanner;
+import java.lang.Math;
 public class areaofequilateraltriangle {
-public static void main(String[] args)
-{
-        Scanner s= new Scanner(System.in);
-        System.out.println("Enter the side of the Triangle:");
-        double a = s.nextDouble();
-        double  area=(Math.sqrt(3)/4)*(a*a);
-        System.out.println("Area of Triangle is: " + area);
+        public static void main(String[] args){
+                Scanner in = new Scanner(System.in);
+                System.out.println("Enter the side of the traingle: ");
+                double a=in.nextInt();
+                double A=Area(a);
         }
+        static double Area(double a){
+             double area=(Math.sqrt(3)/4)*(a*a);
+             return area;
         }
+}
