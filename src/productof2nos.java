eba3d2e09c5ac9c83c3a16f69c1c30a1d7cf416a
2004shashwat17 <114841105+2004shashwat17@users.noreply.github.com>
2023-02-15 14:20:26 +0530

    productof2nos?

--- src/productof2nos.java
+++ src/productof2nos.java
@@ -15,14 +15,42 @@
 //    }
 //}
 //product of two numbers without method?
+//import java.util.Scanner;
+//public class productof2nos {
+//    public static void main(String[] args){
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter 1 and 2 no: ");
+//        int n1 = sc.nextInt();
+//        int n2 = sc.nextInt();
+//        int product= n1*n2;
+//        System.out.println("product of"+product);
+//    }
+//}
+//Take a number as input and print the multiplication table for it.?
+//import java.util.Scanner;
+//public class productof2nos{
+//    public static void main(String []args){
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("entre the number: ");
+//        int num = sc.nextInt();
+//        for(int i=0;i<10;i++){
+//            System.out.println(num+"x"+(i+1)+"="+(num*(i+1)));
+//        }
+//
+//    }
+//}
+//mulitplication table using method?
 import java.util.Scanner;
 public class productof2nos {
-    public static void main(String[] args){
+    public static void main(String []args){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter 1 and 2 no: ");
-        int n1 = sc.nextInt();
-        int n2 = sc.nextInt();
-        int product= n1*n2;
-        System.out.println("product of"+product);
+        int n = sc.nextInt();
+        multiplication(n);
+    }
+    static int multiplication(int n){
+        for(int i=0;i<10;i++){
+            System.out.println(n+"x"+(i+1)+"="+(n*(i+1)));
+        }
+        return n;
     }
 }
