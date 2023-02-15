042c08012f88b78364fc4b2fee3ce2dd55039abe
2004shashwat17 <114841105+2004shashwat17@users.noreply.github.com>
2023-02-15 14:19:41 +0530

    sumof 2nos?

--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,4 @@
 //        System.out.println("Hello world!");
 //    }
 //}
-//Addition of two numbers?
-import java.util.Scanner;
-public class Main{
-    public static void main(String []args){
-        Scanner in =new Scanner(System.in);
-        System.out.println("Enter number1: ");
-        int n1 = in.nextInt();
-        System.out.println("Enter number2: ");
-        int n2 = in.nextInt();
-        int sum = n1+n2;
-        System.out.println("Sum of 2 numbers: "+sum);
-    }
-}
+
