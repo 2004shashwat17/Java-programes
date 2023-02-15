e601d24174dbd320e194c0be22ae36a2de8fbad0
2004shashwat17 <114841105+2004shashwat17@users.noreply.github.com>
2023-02-15 14:18:07 +0530

    Area of rectangle?

--- src/areaofreactangle.java
+++ src/areaofreactangle.java
@@ -1,17 +1,18 @@
 //area of rectangle
-import java.util.Scanner;
-public class areaofreactangle {
-    public static void main(String[] args) {
-        int length;
-        int breath;
-        int area;
-        Scanner in=new Scanner(System.in);
-        System.out.println("entre l");
-        length=in .nextInt();
-        System.out.println("entre b");
-        breath=in .nextInt();
-        area=length*breath;
-        System.out.println("Area of the triangle:"+area);
-
-    }
-}
+//import java.util.Scanner;
+//public class areaofreactangle {
+//    public static void main(String[] args) {
+//        int length;
+//        int breath;
+//        int area;
+//        Scanner in=new Scanner(System.in);
+//        System.out.println("entre l");
+//        length=in .nextInt();
+//        System.out.println("entre b");
+//        breath=in .nextInt();
+//        area=length*breath;
+//        System.out.println("Area of the triangle:"+area);
+//
+//    }
+//}
+//area of triangle?
