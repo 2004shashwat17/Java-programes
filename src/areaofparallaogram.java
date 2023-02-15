39288214941b6d94bc4c4f0bbd0e89f91754469f
2004shashwat17 <114841105+2004shashwat17@users.noreply.github.com>
2023-02-15 14:17:42 +0530

    Area of parallaogram triangle?

--- src/areaofparallaogram.java
+++ src/areaofparallaogram.java
@@ -1,16 +1,35 @@
 //area of parallaogram
+//import java.util.Scanner;
+//public class areaofparallaogram {
+//    public static void main(String[] args) {
+//        int Base;
+//        int Height;
+//        int area;
+//        Scanner in =new Scanner(System.in);
+//        System.out.println("Entre Base");
+//        Base=in.nextInt();
+//        System.out.println("Entre Height");
+//        Height=in.nextInt();
+//        area=Base*Height;
+//        System.out.println("Area of Parallaogram:"+area);
+//    }
+//}
+//area of parallaogram using methods?
 import java.util.Scanner;
 public class areaofparallaogram {
-    public static void main(String[] args) {
-        int Base;
-        int Height;
-        int area;
-        Scanner in =new Scanner(System.in);
-        System.out.println("Entre Base");
-        Base=in.nextInt();
-        System.out.println("Entre Height");
-        Height=in.nextInt();
-        area=Base*Height;
-        System.out.println("Area of Parallaogram:"+area);
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the base: ");
+        int Base = in.nextInt();
+        System.out.println("Enter the Height: ");
+        int Height = in.nextInt();
+        int area = A(Base,Height);
+        System.out.println("area of the parallaogram:"+area);
     }
+    static int A(int Base,int Height){
+        int area = Base*Height;
+        return area;
+
+    }
+
 }
