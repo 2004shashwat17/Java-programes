bcf72f28472d07c9aacf9f59d1aee6fa27f2d0c6
2004shashwat17 <114841105+2004shashwat17@users.noreply.github.com>
2023-02-15 14:17:02 +0530

    Area of isoceles triangle?

--- src/areaofisocelestriangle.java
+++ src/areaofisocelestriangle.java
@@ -1,17 +1,37 @@
 //area of isoceles triangle?
+import org.w3c.dom.ls.LSOutput;
+
 import java.sql.SQLOutput;
 import java.util.Scanner;
+import java.lang.Math;
+//public class areaofisocelestriangle {
+//    public static void main(String[] args) {
+//        Scanner sc=new Scanner(System.in);
+//        System.out.println("Enter the length of the same Side");
+//        double a= sc.nextDouble();
+//
+//        System.out.println("Enter the length of the second Side");//A = ½[√(a2 − b2 ⁄4) × b]
+//        double b = sc.nextDouble();
+//
+//        double area=(b/4)*Math.sqrt((4*a*a-(b*b)));
+//        System.out.println("area of isoceles triangle is"+area);
+//
+//    }
+//}
+//arae of isoceles triangle using methods?
+import java.util.Scanner;
 public class areaofisocelestriangle {
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter the length of the same Side");
-        double a= sc.nextDouble();
-
-        System.out.println("Enter the length of the second Side");
-        double b = sc.nextDouble();
-
+        int a = sc.nextInt();
+        System.out.println("Enter the length of second side");
+        int b = sc.nextInt();
+        double area=A(a,b);
+        System.out.println("area of isocelestriangle"+area);
+    }
+    static double A(int a,int b){
         double area=(b/4)*Math.sqrt((4*a*a-(b*b)));
-        System.out.println("area of isoceles triangle is"+area);
-
+        return area;
     }
 }
