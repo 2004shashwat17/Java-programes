db576ca95fbad2354f8f5da52463ceffd27ec8aa
2004shashwat17 <114841105+2004shashwat17@users.noreply.github.com>
2023-02-19 22:13:55 +0530

    commisionpercentage?

--- src/commisionpercentage.java
+++ src/commisionpercentage.java
@@ -1,14 +1,30 @@
 //commision percentage
+//import java.util.Scanner;
+//public class commisionpercentage {
+//    public static void main(String[] args) {
+//        Scanner in = new Scanner(System.in);
+//        System.out.println("Enter amount: ");
+//        double amount= in.nextDouble();
+//        System.out.println("Enter commision percentage: ");
+//        double cp=in.nextDouble();//cp=commision percentage
+//        double commision = (cp/100)*amount;
+//        System.out.println("commision amount= "+commision);
+//
+//    }
+//}
 import java.util.Scanner;
 public class commisionpercentage {
-    public static void main(String[] args) {
+    public static void main(String []args){
         Scanner in = new Scanner(System.in);
         System.out.println("Enter amount: ");
-        double amount= in.nextDouble();
-        System.out.println("Enter commision percentage: ");
-        double cp=in.nextDouble();//cp=commision percentage
+        double amount = in.nextInt();
+        System.out.println("Entre commision percentage: ");
+        double cp = in.nextInt();
+        commpercen(amount,cp);
+    }
+    static double commpercen(double amount,double cp){
         double commision = (cp/100)*amount;
-        System.out.println("commision amount= "+commision);
-
+        System.out.println("commision amount="+commision);
+        return commision;
     }
 }
