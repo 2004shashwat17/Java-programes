f2bdf2a6668c92d0d514b0082b8b04b4be0e8704
2004shashwat17 <114841105+2004shashwat17@users.noreply.github.com>
2023-02-19 22:14:26 +0530

    compoundInterest?

--- src/compoundInterest.java
+++ src/compoundInterest.java
@@ -1,19 +1,39 @@
-
-// compound interest - Principle*(1+(rate/100))^time
+//
+//// compound interest - Principle*(1+(rate/100))^time
+//import java.lang.*;
+//import java.util.Scanner;
+//public class compoundInterest {
+//    public static void main(String[] args) {
+//        double principle,time,rate,compoundinterest;
+//        Scanner in =new Scanner(System.in);
+//        System.out.println("Enter a principle number: ");
+//        principle = in.nextInt();
+//        System.out.println("Enter the rate of interest: ");
+//        rate = in.nextInt();
+//        System.out.println("Enter the time period in years: ");
+//        time = in.nextInt();
+//        compoundinterest=principle*(Math.pow((1+rate/100),time));
+//        System.out.println("The compound Interest is:"+compoundinterest);
+//
+//    }
+//}
+//compound interest using method?
 import java.lang.*;
 import java.util.Scanner;
 public class compoundInterest {
-    public static void main(String[] args) {
-        double principle,time,rate,compoundinterest;
-        Scanner in =new Scanner(System.in);
+    public static void main(String []args){
+        Scanner in = new Scanner(System.in);
         System.out.println("Enter a principle number: ");
-        principle = in.nextInt();
+        double prin=in.nextInt();
         System.out.println("Enter the rate of interest: ");
-        rate = in.nextInt();
+        double rate =in.nextInt();
         System.out.println("Enter the time period in years: ");
-        time = in.nextInt();
-        compoundinterest=principle*(Math.pow((1+rate/100),time))-principle;
-        System.out.println("The compound Interest is:"+compoundinterest);
-
+        double time = in.nextInt();
+        cp( prin, rate, time);
+    }
+    static double cp(double prin,double rate,double time){
+        double compoundInte = prin*(Math.pow((1+rate/100),time));
+    System.out.println("The compound Interest is: "+compoundInte);
+    return compoundInte;
     }
 }
