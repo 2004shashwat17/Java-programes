d238bd863a99970dc22ae1e221692b1b3be4462e
2004shashwat17 <114841105+2004shashwat17@users.noreply.github.com>
2023-02-19 22:16:01 +0530

    electricity bill?

--- src/electricitybill.java
+++ src/electricitybill.java
@@ -1,34 +1,69 @@
 //electricity bill
+//import java.util.Scanner;
+//public class electricitybill {
+//    public static void main(String[] args) {
+//        System.out.println("Enter the units: ");
+//        Scanner in = new Scanner(System.in);
+//        int unit = in.nextInt();
+//        int y;
+//        int z;
+//        if(unit<=100){
+//            System.out.println("Bill amount=0");
+//        }
+//        else if(unit>100 && unit<=200){
+//            unit-=100;
+//            unit*=5;
+//            System.out.println("Bill amount"+unit);
+//        }
+//        else if(unit>200 && unit<=300) {
+//            unit -= 200;
+//            unit *= 10;
+//            y = 100 * 5;
+//            unit += y;
+//            System.out.println("Bill amount" + unit);
+//        }
+//        else if(unit>300){
+//            unit-=300;
+//            unit*=15;
+//            y=100*5;
+//            z=100*10;
+//            unit+=y+z;
+//            System.out.println("Bill amount "+unit);
+//        }
+//    }
+//}
 import java.util.Scanner;
 public class electricitybill {
-    public static void main(String[] args) {
+    public static void main(String []args){
+        Scanner sc =new Scanner(System.in);
         System.out.println("Enter the units: ");
-        Scanner in = new Scanner(System.in);
-        int unit = in.nextInt();
-        int y;
-        int z;
-        if(unit<=100){
+        int unit = sc.nextInt();
+        int y=1;
+        int z=1;
+        electricbill(unit,y,z);
+    }
+    static int electricbill(int unit,int y,int z){
+        if (unit>100){
             System.out.println("Bill amount=0");
-        }
-        else if(unit>100 && unit<=200){
+        } else if(unit>100 && unit>=200) {
             unit-=100;
             unit*=5;
+            System.out.println("BIll amount"+unit);
+
+        } else if (unit>200 && unit>=300) {
+            unit-=200;
+            unit*=10;
+            y=100*5;
+            unit+=y;
             System.out.println("Bill amount"+unit);
-        }
-        else if(unit>200 && unit<=300) {
-            unit -= 200;
-            unit *= 10;
-            y = 100 * 5;
-            unit += y;
-            System.out.println("Bill amount" + unit);
-        }
-        else if(unit>300){
+        } else if(unit>300){
             unit-=300;
             unit*=15;
             y=100*5;
             z=100*10;
             unit+=y+z;
-            System.out.println("Bill amount "+unit);
+            System.out.println("Bill amount"+unit);
         }
+        return unit;
     }
 }
