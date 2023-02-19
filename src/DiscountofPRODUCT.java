1d49d2a80bc1827162d5a30a2764ea1719206b7c
2004shashwat17 <114841105+2004shashwat17@users.noreply.github.com>
2023-02-19 22:15:06 +0530

    DiscountofPRODUCT?

--- src/DiscountofPRODUCT.java
+++ src/DiscountofPRODUCT.java
@@ -1,16 +1,34 @@
 //Discount of products
+////import java.util.Scanner;
+//public class DiscountofPRODUCT {
+//    public static void main(String[] args) {
+//        double dis,Amount,marketprice,s;
+//        Scanner in = new Scanner (System.in);
+//        System.out.println("Enter market price: ");
+//        marketprice= in.nextDouble();
+//        System.out.println("Enter discount percentage: ");
+//        dis=in.nextInt();
+//        s=100-dis;
+//        Amount=(s*marketprice)/100;
+//        System.out.println("amount after discount="+ Amount);
+//
+//    }
+//}
+//Discounts of products using methods?
 import java.util.Scanner;
-public class DiscountofPRODUCT {
-    public static void main(String[] args) {
-        double dis,Amount,marketprice,s;
-        Scanner in = new Scanner (System.in);
+public class DiscountofPRODUCT{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter market price: ");
-        marketprice= in.nextDouble();
+        int mp = sc.nextInt();
         System.out.println("Enter discount percentage: ");
-        dis=in.nextInt();
-        s=100-dis;
-        Amount=(s*marketprice)/100;
-        System.out.println("amount after discount="+ Amount);
-
+        int dis = sc.nextInt();
+        int s=1;
+        marketprice( mp, dis, s);
+        }
+        static int marketprice(int mp,int dis,int s){
+        int Amount = (100-dis)*mp/100;
+        System.out.println("amount after discount: "+Amount);
+        return Amount;
     }
 }
