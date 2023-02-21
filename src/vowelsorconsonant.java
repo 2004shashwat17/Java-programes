0ebc275fca6430f4174387a4f0be75c765640e75
2004shashwat17 <114841105+2004shashwat17@users.noreply.github.com>
2023-02-21 12:54:51 +0530

    vowels or consonant?

--- src/vowelsorconsonant.java
+++ src/vowelsorconsonant.java
@@ -1,17 +1,34 @@
 //vowels or consonant?
+//import java.util.Scanner;
+//public class vowelsorconsonant {
+//    public static void main(String[] args) {
+//
+//
+//        System.out.println("Enter any character: ");
+//        Scanner in = new Scanner(System.in);
+//        char ch=in.next().charAt(0);
+//        if( ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u' ){
+//            System.out.println("VOWEL"+ch);
+//        }
+//        else{
+//            System.out.println("CONSONANT: "+ch);
+//        }
+//    }
+//}
+//vowels or consonants using methods?
 import java.util.Scanner;
 public class vowelsorconsonant {
     public static void main(String[] args) {
-
-        char ch;
-        System.out.println("Enter any character: ");
-        Scanner in = new Scanner(System.in);
-        ch=in.next().charAt(0);
-        if( ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u' ){
-            System.out.println("VOWEL"+ch);
+        Scanner sc = new Scanner(System.in);
+       char ch=sc.next().charAt(0);
+       vorc(ch);
+    }
+    static void vorc(char ch){
+        if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u') {
+            System.out.println("Vowels: " + ch);
         }
-        else{
-            System.out.println("CONSONANT: "+ch);
+            else {
+                System.out.println("consonant: " + ch);
+            }
         }
     }
-}
