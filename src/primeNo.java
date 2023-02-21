5ae5e2f755d1f9578299cf63d365d82fc0d526ce
2004shashwat17 <114841105+2004shashwat17@users.noreply.github.com>
2023-02-21 12:55:53 +0530

    prime no ?

--- src/primeNo.java
+++ src/primeNo.java
@@ -18,3 +18,27 @@
 //    }
 //}
 //using methods?
+import java.util.Scanner;
+public class primeNo{
+    public static void main(String []args){
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int temp=0;
+        primeno(num,temp);
+    }
+    static int primeno(int num,int temp){
+        for(int i=2;i<num-1;i++){
+            if(num%i==0) {
+                temp = temp + i;
+            }
+            if(temp>0){
+                System.out.println("It is not a prime no. ");
+            }
+            else{
+                System.out.println("It is a prime no. ");
+            }
+        }
+        return temp;
+    }
+}
+//prime no between two nos?
