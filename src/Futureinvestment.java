7fca331bf6dfdae58f9c631107fb4de0f53cdcce
2004shashwat17 <114841105+2004shashwat17@users.noreply.github.com>
2023-02-21 12:58:05 +0530

    future investment?

--- src/Futureinvestment.java
+++ src/Futureinvestment.java
@@ -4,18 +4,20 @@
 //pv=present investment
 //i=interest paid by the investment
 //n= no.of period the investment will be held
-import java.lang.*;
+//import java.lang.*;
+//import java.util.Scanner;
+//public class Futureinvestment {
+//    public static void main(String[] args) {
+//        Scanner in =new Scanner(System.in);
+//        System.out.println("Enter the pv: ");
+//        double pv= in.nextInt();
+//        System.out.println("Enter the interest: ");
+//        double i=in.nextInt();
+//        System.out.println("Enter the no.of periods: ");
+//        double N=in.nextInt();
+//        double fv=pv*Math.pow((1+i/100),N);
+//        System.out.println("Future investment: "+fv);
+//    }
+//}
+//future iunvestment using methods?
 import java.util.Scanner;
-public class Futureinvestment {
-    public static void main(String[] args) {
-        Scanner in =new Scanner(System.in);
-        System.out.println("Enter the pv: ");
-        double pv= in.nextInt();
-        System.out.println("Enter the interest: ");
-        double i=in.nextInt();
-        System.out.println("Enter the no.of periods: ");
-        double N=in.nextInt();
-        double fv=pv*Math.pow((1+i/100),N);
-        System.out.println("Future investment: "+fv);
-    }
-}
