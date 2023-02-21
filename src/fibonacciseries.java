196fc3b1217c8a0e4607e3dc0293e5c41390b6f6
2004shashwat17 <114841105+2004shashwat17@users.noreply.github.com>
2023-02-21 12:58:33 +0530

    fibonacci series?

--- src/fibonacciseries.java
+++ src/fibonacciseries.java
@@ -1,22 +1,43 @@
 //fibonacciseries
+//import java.util.Scanner;
+//public class fibonacciseries {
+//    public static void main(String[]args) {
+//
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//        int a = 0;
+//        int b = 1;
+//        //ravi
+//        int count = 2;
+//        while (count <= n) {
+//            int temp=b;
+//            b=b+a;
+//            a=temp;
+//            count++;
+//
+//        }
+//        System.out.print(b);
+//
+//    }
+//}
 import java.util.Scanner;
 public class fibonacciseries {
-    public static void main(String[]args) {
-
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[]args){
+        Scanner sc= new Scanner(System.in);
         int n = sc.nextInt();
-        int a = 0;
-        int b = 1;
-        //ravi
-        int count = 2;
-        while (count <= n) {
-            int temp=b;
+        int a=0;
+        int b=1;
+        int count =2;
+        fibonnaci(a,b,count,n);
+    }
+    static int fibonnaci(int a,int b,int count,int n){
+        int temp = b;
+        while(count<=n){
             b=b+a;
             a=temp;
             count++;
 
         }
-        System.out.print(b);
-
+        return b;
     }
 }
