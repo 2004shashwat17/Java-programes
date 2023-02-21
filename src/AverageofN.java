2867249d6b6bfd35e59818a8577ef09c4f92d284
2004shashwat17 <114841105+2004shashwat17@users.noreply.github.com>
2023-02-21 12:59:11 +0530

    average of n numbers?

--- src/AverageofN.java
+++ src/AverageofN.java
@@ -1,17 +1,35 @@
 //avergae ofn number
+//import java.util.Scanner;
+//public class AverageofN {
+//    public  static void  main(String[] args){
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("enter the no.");
+//                int num= sc.nextInt();
+//        int i;
+//        float Total =0;
+//        for(i=1;i<=num;i++){
+//            System.out.println("Enter the numbers"+i);
+//            int number= sc.nextInt();
+//            Total = number+Total;
+//        }
+//        System.out.println("the avg is: "+Total/num);
+//    }
+//}
+//avr age of n nos using methods?
 import java.util.Scanner;
 public class AverageofN {
-    public  static void  main(String[] args){
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.println("enter the no.");
-                int num= sc.nextInt();
-        int i;
-        float Total =0;
-        for(i=1;i<=num;i++){
-            System.out.println("Enter the numbers"+i);
-            int number= sc.nextInt();
-            Total = number+Total;
+        System.out.println("Enter the no.");
+        int num = sc.nextInt();
+        float Total = average(num);
+        System.out.println(Total/num);
+    }
+    static float average(int num) {
+        float total=0;
+        for (int i = 0; i <= num; i++) {
+            total = i + total;
         }
-        System.out.println("the avg is: "+Total/num);
+        return total;
     }
 }
